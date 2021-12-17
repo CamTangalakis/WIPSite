29ce8ce63f0e82f62b60d48ad849e072cace2381
Cam Tangalakis-Lippert <cami.lippert@gmail.com>
2021-12-16 20:36:55 -0800

    styling navbar

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -11,6 +11,19 @@ const HomePage = () => {
     projects = projects.projects
     const User = useSelector(state => state.session.user)
 
+    const words = ['work', 'project', 'craft', 'design', 'plan']
+    let i = 0
+    const increment = () => {
+        setInterval(() => {
+            i += 1
+            if (i >= 5) i = 0
+            // console.log(i)
+            return i
+        }, 2000, i)
+    }
+    increment()
+    let word = words[i]
+
     useEffect(()=>{
         const func = async() => {
             await dispatch(getProjects())
@@ -21,7 +34,7 @@ const HomePage = () => {
     return (
         <div className='homePageContainer'>
             <div className='homeHeaderContainer'>
-                <h1 className='wipHeader'>everything is a work in progress...</h1>
+                <h1 className='wipHeader'>everything is a {word} in progress...</h1>
             </div>
 
             <div>
@@ -29,7 +42,7 @@ const HomePage = () => {
                 <div className='userOptionsContainer'>
                     <img className='userHeader' src={User.profilePic}></img>
                     <div className='userButtons'>
-                        <NavLink to='/newProject'>Start a Project</NavLink>
+                        <NavLink to='/newProject' className='startProject'>Start a Project</NavLink>
                         <button type='button' className='userButton'>Search Projects</button>
                     </div>
                 </div>
