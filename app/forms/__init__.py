e39cd77f7e4d6ec94cab290d89e30e9b247f9a22
Cam Tangalakis-Lippert <cami.lippert@gmail.com>
2022-01-27 14:57:06 -0800

    "fixing stuff"
    Revert "ruined"
    
    <commit>
    
    This reverts commit a0123ddbe6ab59c8401da534fbf05d2a0242d98c.

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -4,4 +4,3 @@ from .new_comment_form import NewCommentForm, EditCommentForm
 from .new_fav_form import NewFavForm
 from .new_project_form import NewProjectForm, EditProjectForm
 from .new_album_form import NewAlbumForm
-from .new_post_form import NewPostForm
--- app/forms/new_post_form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class NewPostForm(FlaskForm):
-    title = StringField('title', validators=[DataRequired()])
-    content = StringField('content', validators=[DataRequired()])
-    photo = StringField('photo', validators=[DataRequired()])
